5d61108e6798d87acbd926469ae5358c3b282fb7
SufBit <sufarshad.99@gmail.com>
2024-05-01 18:26:58 -0500

    removed tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router} from 'react-router-dom'; // Importing BrowserRouter, Switch, and Route
 import Navbar from './Components/Navbar'; // Importing the Navbar component
-import OvalPicture from './Components/Oval'; // Importing the Oval component
 import AboutMe from './Components/AboutMe';
 
 function App() {
