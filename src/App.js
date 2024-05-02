ecf84b17b0bc1c4dff6e6d00a24b6ff60a3cd3c0
SufBit <sufarshad.99@gmail.com>
2024-05-01 22:17:22 -0500

    added typing behavior

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router} from 'react-router-dom'; // Importing BrowserRouter, Switch, and Route
 import Navbar from './Components/Navbar'; // Importing the Navbar component
 import AboutMe from './Components/AboutMe';
+import TypingText from './Components/TypingText';
+
 
 function App() {
   return (
@@ -9,6 +11,9 @@ function App() {
       <Router>
         <div>
           <Navbar /> {/* Including the Navbar component */}
+          <div style={{ marginLeft: '20px' }}> {/* Adjust margin as needed */}
+            <TypingText /> {/* Include the TypingText component */}
+          </div>
           <AboutMe />
         </div>
       </Router>
---
+++ src/Components/TypingText.js
@@ -0,0 +1,55 @@
+import React, { useMemo, useState, useEffect } from 'react';
+
+const TypingText = () => {
+  const [textIndex, setTextIndex] = useState(0);
+  const texts = useMemo(() => ["DevOps Engineer", "Full-Stack Developer ", "Software Architect"],[]); // Add more texts as needed
+  const [displayText, setDisplayText] = useState(texts[0]);
+
+  useEffect(() => {
+    const typingInterval = setInterval(() => {
+      setTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, 3000); // Change text every 2 seconds, adjust as needed
+
+    return () => clearInterval(typingInterval);
+  }, [texts.length]);
+
+  useEffect(() => {
+    let index = 0;
+    const typingTimeout = setTimeout(() => {
+        const typingInterval = setInterval(() => {
+            if (index <= texts[textIndex].length) {
+              setDisplayText(texts[textIndex].substring(0, index));
+              index++;
+            } else {
+              clearInterval(typingInterval);
+            }
+          }, 100); // Typing speed, adjust as needed
+        }, 500);// Typing speed, adjust as needed
+
+    return () => clearTimeout(typingTimeout);
+  }, [textIndex, texts]);
+
+  return ( 
+        
+    <div style={{ 
+        position: 'absolute',
+        top: '3%',
+        left: '-3%',
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        textAlign: 'center',
+        width: '50%',
+        height: '100vh',
+        marginLeft: '0px', // Adjust margin as needed
+        fontSize: '24px', // Adjust font size as needed
+      }}>
+        <div style={{ color: 'black' }}>I am a</div>
+        <div style={{ color: 'red', fontWeight: 'bold',}}>{displayText}</div>
+      </div>
+
+    );
+};
+
+export default TypingText;
