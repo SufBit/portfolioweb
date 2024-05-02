520bf18a44a11b98b182a6d806d9bb13f835da87
SufBit <sufarshad.99@gmail.com>
2024-05-02 16:02:11 -0500

    m

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -5,7 +5,7 @@ const AboutMe = () => {
   return (
     <div className="about-me">
       <h2>About Me</h2>
-      <img src="portfolioweb/profile.png" alt="Profile" className="profile" />
+      <img src="../portfolioweb/profile.png" alt="Profile" className="profile" />
       <p>I'm a passionate Software Developer graduating in May 2024 with a Bachelors of Science in Computer Science from the University of North Texas. With a strong foundation in frontend and backend technologies, including proficiency in JavaScript, Python, and C++, I'm eager to apply my skills and contribute to innovative projects.</p>
 
     </div>
